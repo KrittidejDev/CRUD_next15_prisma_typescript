fcfb01b5957cb15d9cd1aa67de675332f5935a39
KrittidejDev <krittidej.dev@gmail.com>
2025-04-19 10:27:24 +0700

    first

---
+++ app/(root)/posts/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const PostsPage = () => {
+  return (
+    <div className="flex flex-col items-center gap-y-5 pt-24 text-center">
+      <h1 className="text-3xl font-semibold">All Posts (0)</h1>
+
+      <ul className="border-t border-b border-black/10 py-5 leading-8"></ul>
+    </div>
+  );
+};
+
+export default PostsPage;
